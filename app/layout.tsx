30d4e220dbaf1afc658146a898b85594bdb8810c
gustavoalvim41 <gustavoalvim85@gmail.com>
2026-01-03 23:34:50 -0300

    add tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -10,7 +11,8 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
   title: "Carlos MS - Mudas de Seringueira de Alta Qualidade",
-  description: "Até 6 mil mudas por viagem com transporte especializado. Entrega garantida em todo o Brasil. Solicite seu Orçamento!",
+  description:
+    "Até 6 mil mudas por viagem com transporte especializado. Entrega garantida em todo o Brasil. Solicite seu Orçamento!",
 };
 export default function RootLayout({
   children,
@@ -19,9 +21,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={montserrat.className}>
-        {children}
-      </body>
+      {/* Google Tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17848964546"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17848964546');
+            `,
+        }}
+      />
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
